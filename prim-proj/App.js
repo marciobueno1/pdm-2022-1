1135e1d18efbcdc26192b7526c71481d016aec87
Márcio Bueno <marcio.bueno@unicap.br>
2022-03-09 21:59:24 -0300

    Core Components: Image, ScrollView
    SafeAreaView

--- prim-proj/App.js
+++ prim-proj/App.js
@@ -1,28 +1,73 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   const handlePress = () => {
     console.log("O botão foi clicado!");
   };
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Olá Turma!</Text>
-      <Button title="Clique me" onPress={handlePress} color="green" />
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.screen}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.text}>Olá Turma!</Text>
+        <Button title="Clique me" onPress={handlePress} color="green" />
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: "https://reactnative.dev/img/tiny_logo.png",
+          }}
+        />
+        <Image
+          style={styles.imgCafe}
+          source={require("./assets/img/cafe.jpg")}
+          resizeMode="contain"
+        />
+        <View style={styles.vSpace15} />
+        <Image
+          style={styles.imgCafe}
+          source={require("./assets/img/cafe.jpg")}
+          resizeMode="cover"
+        />
+        <StatusBar style="auto" />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: "beige",
+  },
+  container: {
+    padding: 10,
+    backgroundColor: "green",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
     fontSize: 32,
     fontWeight: "bold",
+  },
+  tinyLogo: {
+    width: 100,
+    height: 100,
+  },
+  imgCafe: {
+    width: 300,
+    height: 300,
+    borderColor: "black",
+  },
+  vSpace15: {
+    width: "100%",
+    height: 15,
+    backgroundColor: "yellow",
+    marginVertical: 5,
   },
 });
